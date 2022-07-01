a5e7599c2c828d22f1dd74d11d9c8776cc7c3ae3
hikmanisyariful <syarifulfajar017@gmail.com>
2022-07-01 07:56:11 +0700

    Apply userEvent instead of fireEvent

--- src/pages/summary/SummaryForm.js
+++ src/pages/summary/SummaryForm.js
@@ -1,13 +1,28 @@
 import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import Popover from "react-bootstrap/Popover";
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
 
 export default function SummaryForm() {
   const [tcChecked, setTcChecked] = useState(false);
 
+  const popover = (
+    <Popover id="popover-basic">
+      <Popover.Body>No ice cream will actually be delivered</Popover.Body>
+      {/* note: for React Bootstrap 2.x, the previous line needs to be:
+      <Popover.Body>No ice cream will actually be delivered</Popover.Body>
+      (replace Popover.Content with Popover.Body). For more details, see 
+      https://www.udemy.com/course/react-testing-library/learn/lecture/30126784*/}
+    </Popover>
+  );
+
   const checkboxLabel = (
     <span>
-      I agree to <span style={{ color: "blue" }}> Terms and Conditions</span>
+      I agree to{" "}
+      <OverlayTrigger placement="right" overlay={popover}>
+        <span style={{ color: "blue" }}> Terms and Conditions</span>
+      </OverlayTrigger>
     </span>
   );
 
