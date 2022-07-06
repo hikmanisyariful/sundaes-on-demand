29b91ec66afa052926d7081cfa0be4fb15fb0b28
hikmanisyariful <syarifulfajar017@gmail.com>
2022-07-06 12:18:09 +0700

    Test and code context for toppings subtotal

--- src/pages/entry/tests/totalUpdates.test.js
+++ src/pages/entry/tests/totalUpdates.test.js
@@ -26,3 +26,35 @@ test("update scoop subtotal when scoops change", async () => {
   userEvent.type(chocolateInput, "2");
   expect(scoopsSubtotal).toHaveTextContent("6.00");
 });
+
+test("update topping subtotal when toppings change", async () => {
+  render(<Options optionType="toppings" />, { wrapper: OrderDetailsProvider });
+
+  // make sure total starts out $0.00
+  const toppingsSubtotal = screen.getByText("Toppings total: $", {
+    exact: false,
+  });
+  expect(toppingsSubtotal).toHaveTextContent("0.00");
+
+  // update cherries topping to 3 and check the subtotal
+  const cherriesTopping = await screen.findByRole("spinbutton", {
+    name: "Cherries",
+  });
+  userEvent.clear(cherriesTopping);
+  userEvent.type(cherriesTopping, "3");
+  expect(toppingsSubtotal).toHaveTextContent("4.50");
+
+  // update M&Ms topping to 1 and check the subtotal
+  const mmsTopping = await screen.findByRole("spinbutton", { name: "M&Ms" });
+  userEvent.clear(mmsTopping);
+  userEvent.type(mmsTopping, "1");
+  expect(toppingsSubtotal).toHaveTextContent("6.00");
+
+  // update Hot fudge to 2 and check the subtotal
+  const hotFudgeTopping = await screen.findByRole("spinbutton", {
+    name: "Hot fudge",
+  });
+  userEvent.clear(hotFudgeTopping);
+  userEvent.type(hotFudgeTopping, "2");
+  expect(toppingsSubtotal).toHaveTextContent("9.00");
+});
